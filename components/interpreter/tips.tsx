b4cd5d4249af367096ab7b3a53201b93970b2470
panzerstadt <tliqun@gmail.com>
2023-11-16 00:46:00 +0900

    more programmatic tips component

--- components/interpreter/tips.tsx
+++ components/interpreter/tips.tsx
@@ -1,11 +1,35 @@
+const TIPS = [
+  "arrow-up/down to navigate input history",
+  "cmd + a, arrow-left to go to the beginning",
+  "opt + arrows to jump per-word (whitespace separated)",
+];
+
+const GRADIENT = [
+  "opacity-50",
+  "opacity-40",
+  "opacity-30",
+  "opacity-25",
+  "opacity-20",
+  "opacity-10",
+  "opacity-5",
+];
+
 export const Tips = () => {
   return (
-    <ul className="transition-opacity group-hover:opacity-100 opacity-20">
-      <li>arrow-up/down to navigate input history</li>
-      <li>cmd + a, arrow-left to go to the beginning</li>
-      <li>
-        opt + arrows to jump per-word {"("}whitespace separated{")"}
-      </li>
+    <ul className="select-none">
+      {TIPS.map((t, i) => {
+        const opacity = GRADIENT[i] || GRADIENT[GRADIENT.length - 1];
+        console.log(opacity);
+
+        return (
+          <li
+            key={i}
+            className={`${opacity} group-hover:opacity-100 transition-opacity delay-75 duration-300`}
+          >
+            {t}
+          </li>
+        );
+      })}
     </ul>
   );
 };
