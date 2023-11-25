d8f6ba646b11c5e8e457131c56452e169307c56a
panzerstadt <tliqun@gmail.com>
2023-11-25 19:31:02 +0900

    Update page layout padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-center px-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-center px-4 sm:px-24 ${inter.className}`}
     >
       <div className="h-[80dvh] w-full">
         <Interpreter />
